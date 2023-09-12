0edd4023698f7d967f0d090cc539c0e2df9b926b
박인국 <pkook7@gmail.com>
2023-09-12 13:42:44 +0900

    input button 수정

--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -1,130 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Button from '../../components/Button/Button';
 import './LogIn.scss';
-import Input from '../../components/Input/Input';
-import { useNavigate } from 'react-router-dom';
 
 const LogIn = () => {
-  const [inputs, setInputs] = useState({
-    email: '',
-    password: '',
-  });
-
-  // useNavigate
-  const navigate = useNavigate();
-  const navigateSignUp = () => {
-    navigate('/signup ');
-  };
-
-  const navigateLogIn = () => {
-    fetch('http://localhost:8000/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-      body: JSON.stringify({
-        email: userId,
-        password: userPassword,
-        name: '박경재',
-      }),
-    })
-      .then(res => res.json())
-      .then(result => {
-        console.log(result);
-      });
-  };
-
-  // Logo
-  const MainLogo = process.env.PUBLIC_URL + '/images/Weread/Logo.png';
-  const WordLogo = process.env.PUBLIC_URL + '/images/Weread/logo_wecode.png';
-
-  // State
-  const [userId, setUserId] = useState('');
-  const [userPassword, setUserPassword] = useState('');
-
-  // 이메일 검증식
-  const emailRegEx =
-    /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/i;
-  const emailCheck = username => {
-    return emailRegEx.test(username); //형식에 맞을 경우, true 리턴
-  };
-
-  // 비밀번호 검증식
-  const passwordCheck = password => {
-    if (password.length >= 10) {
-      return true;
-    } else return false;
-  };
-
-  // Input 값 저장
-  const onInputChange = event => {
-    const { name, value } = event.target;
-    setInputs(prev => ({ ...prev, [name]: value }));
-  };
-
+  const publicUrl = process.env.PUBLIC_URL;
   return (
-    <div className="login">
-      <Input
-        placeholder="이메일"
-        name="email"
-        type="text"
-        onInputChange={onInputChange}
-        inputs={inputs}
-        disabled={false}
-        dataCheck={false}
-        status="normal"
-        box={true}
-        title="기본정보"
-        required="필수 사항"
-      />
-      <Input
-        placeholder="비밀번호"
-        name="password"
-        type="password"
-        onInputChange={onInputChange}
-        inputs={inputs}
-        disabled={false}
-        status="normal"
-        box={false}
-        title=""
-        required=""
-      />
-      <Input
-        placeholder="이메일"
-        name="email"
-        type="text"
-        onInputChange={onInputChange}
-        inputs={inputs}
-        disabled={true}
-        dataCheck={false}
-        status="normal"
-        box={false}
-        title=""
-        required=""
-      />
-      <Input
-        placeholder="비밀번호"
-        type="password"
-        name="password"
-        onInputChange={onInputChange}
-        disabled={false}
-        dataCheck={false}
-        status="error"
-        box={false}
-        title=""
-        required=""
-      />
-      <Input
-        placeholder="비밀번호"
-        type="password"
-        name="password"
-        onInputChange={onInputChange}
-        disabled={false}
-        dataCheck={false}
-        status="done"
-        box={false}
-        title=""
-        required=""
-      />
+    <div className="logIn">
+      <section className="splash">
+        <hgroup>
+          <h1>
+            <img src={`${publicUrl}/images/Logo.svg`} alt="로고" />
+          </h1>
+          <h2>
+            <img
+              src={`${publicUrl}/images/logo_wecode.svg`}
+              alt="위코드 로고"
+            />
+          </h2>
+        </hgroup>
+      </section>
+      <section className="container">
+        <form className="form">
+          <fieldset>
+            <legend className="hidden">로그인 양식</legend>
+            <Button
+              type="submit"
+              shape="solid"
+              scale="large"
+              text="로그인"
+              disabled
+            />
+            <Button shape="outline" scale="small" text="글쓰기" />
+            <Button shape="icon" icon="like" />
+            <Button shape="mix" text="뒤로" />
+          </fieldset>
+        </form>
+      </section>
     </div>
   );
 };
